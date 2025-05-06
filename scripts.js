4e05521473118cc52c78b469978c35dc735ebd81
RaulSuero <raulsuerogarcia@gmail.com>
2025-05-06 16:08:27 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    // Burguer
+    var menu = document.querySelector('.hamburger');
+
+    function toggleMenu (event) {
+    this.classList.toggle('is-active');
+    document.querySelector( ".menuppal" ).classList.toggle("is_active");
+    event.preventDefault();
+    }
+
+    menu.addEventListener('click', toggleMenu, false);
+
+});
---
+++ test/testscripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const nav = document.getElementById('nav');
+    
+    menuToggle.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+    
+});
